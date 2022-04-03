8a93c9f32385b0016c076f3f6b49ab71a59ab541
marynag <mgrabchak1109@gmail.com>
2022-04-03 21:49:45 +0300

    add colours

--- try1.js
+++ try1.js
@@ -1,6 +1,150 @@
 var readlineSync = require('readline-sync');
+var blessed = require('blessed');
+
+
+/// Create a screen object.
+var screen = blessed.screen({
+	smartCSR: true
+  });
+  
+  
+  // Create a box perfectly centered horizontally and vertically.
+  var box = blessed.box({
+	top: 'left',
+	left: '35%',
+	width: '15%',
+	height: '40%',
+	tags: true,
+	border: {
+	  type: 'line'
+	},
+	style: {
+	  fg: 'white',
+	  bg: 'green',
+	  border: {
+		fg: 'white'
+	  },
+	}
+  });
+  
+  var box1 = blessed.box({
+	top: 'left',
+	left: 'right',
+	width: '15%',
+	height: '40%',
+	tags: true,
+	border: {
+	  type: 'line'
+	},
+	style: {
+	  fg: 'white',
+	  bg: 'blue',
+	  border: {
+		fg: 'white'
+	  },
+	}
+  });
+
+  var box2 = blessed.box({
+	top: '50%-1',
+	left: 'right',
+	width: '15%',
+	height: '40%',
+	tags: true,
+	border: {
+	  type: 'line'
+	},
+	style: {
+	  fg: 'white',
+	  bg: 'red',
+	  border: {
+		fg: 'white'
+	  },
+	}
+  });
+
+  var box3 = blessed.box({
+	top: '50%-1',
+	left: '35%',
+	width: '15%',
+	height: '40%',
+	tags: true,
+	border: {
+	  type: 'line'
+	},
+	style: {
+	  fg: 'white',
+	  bg: 'yellow',
+	  border: {
+		fg: 'white'
+	  },
+	}
+  });
+
+  var box4 = blessed.box({
+	top: 'right',
+	left: '65%',
+	width: '15%',
+	height: '40%',
+	tags: true,
+	border: {
+	  type: 'line'
+	},
+	style: {
+	  fg: 'white',
+	  bg: 'cyan',
+	  border: {
+		fg: 'white'
+	  },
+	}
+  });
+
+  var box5 = blessed.box({
+	top: '50%-1',
+	left: '65%',
+	width: '15%',
+	height: '40%',
+	tags: true,
+	border: {
+	  type: 'line'
+	},
+	style: {
+	  fg: 'white',
+	  bg: 'magenta',
+	  border: {
+		fg: 'white'
+	  },
+	}
+  });
+  
+  
+  screen.append(box);
+  screen.append(box1);
+  screen.append(box2);
+  screen.append(box3);
+  screen.append(box4);
+  screen.append(box5);
+  
+  
+  var icon = blessed.image({
+	parent: box,
+	top: 0,
+	left: 0,
+	type: 'overlay',
+	width: 'shrink',
+	height: 'shrink',
+	file: __dirname + '/my-program-icon.png',
+	search: false
+  }); 
+  
+  
+  screen.render();
+
+
+console.log(" \r\n");
 
 var counter=0;
+
 
 function randomNumber(){             
     var number= Math.floor(Math.random()*10);
@@ -21,6 +165,7 @@ function computerNumber(){
     console.log("Загадане число "+rnd);
     return rnd;
 }
+
 
 
 function verifyEntry(number){
@@ -74,10 +219,9 @@ function check(){
 	}
 };
 
-
-
 var goal = check();
 
 for(let r=0; rnd != userNumber;r++ ){
 	var goal = check();
 }
+
