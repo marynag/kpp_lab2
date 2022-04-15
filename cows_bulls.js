01f0922144e9caed8923a6d6dc68507db52dd7ba
marynag <mgrabchak1109@gmail.com>
2022-04-15 18:17:38 +0300

    Color game

---
+++ cows_bulls.js
@@ -0,0 +1,151 @@
+
+var readlineSync = require('readline-sync');
+/*
+
+console.log(" \r\n");
+
+var counter=0;
+
+var color_num_first=3;
+var color_num_second=3;
+var color_num_third=4;
+var color_num_fourth=1;
+
+var rnd=color_num_first.toString()+color_num_second.toString()+color_num_third+color_num_fourth.toString();
+
+console.log("Загадане число "+rnd);
+
+function verifyEntry(number){	
+	verif=true;
+	number=number.toString();	
+	if (number.length!=4){
+		verif=false;
+	}
+	return verif;
+}
+
+function check(){  	
+
+	userNumber = readlineSync.question('Enter number: ');
+
+	var cows=0;
+	var bulls=0;
+
+	if (verifyEntry(userNumber)===false){
+		console.log(userNumber+" - Неправильні дані\r\n");
+	} 
+	else{
+		rnd=rnd.toString();
+	
+		for (let n=0; n<4; n++){
+			for (let i=0; i<4; i++){
+				if (userNumber.charAt(i) === rnd.charAt(n) && i==n){
+					bulls++;
+				}
+				else {
+					if(userNumber.charAt(i) === rnd.charAt(n)) {
+						cows++;
+					}
+				}
+			}
+		}
+		counter++;
+		if (bulls===4){
+			console.log(userNumber+" - Загадане число"+" \r\n"+ "Ви виграли, кількість спроб: "+counter+" \r\n");
+
+			counter=0;
+    	}
+		else{
+        	console.log(userNumber+ " - " + cows +" корів, "+ bulls +" биків, спроб: "+counter+"\r\n");}
+	}
+};
+
+var goal = check();
+
+for(let r=0; rnd != userNumber;r++ ){
+		var goal = check();
+	}
+*/
+
+console.log(" \r\n");
+
+var counter=0;
+
+
+function randomNumber(){             
+    var number= Math.floor(Math.random()*10);
+    return number
+}
+
+
+function computerNumber(){
+    var fourth= randomNumber();
+    for (var i;i=1;i++) {
+	    var third= randomNumber();
+	    var second= randomNumber();
+	    var first= randomNumber();
+	    if(third!=fourth && second!=fourth && second!=third && first!=fourth && first!=third && first!=second)
+        {break}
+    }
+    var rnd=fourth.toString()+third.toString()+second.toString()+first.toString();
+    console.log("Загадане число "+rnd);
+    return rnd;
+}
+
+
+
+function verifyEntry(number){	
+	verif=true;
+	number=number.toString();	
+	if (number.length!=4){
+		verif=false;
+	}
+	return verif;
+}
+
+
+rnd = computerNumber();
+
+rnd='3341'
+function check(){  	
+
+	userNumber = readlineSync.question('Enter number: ');
+
+	var cows=0;
+	var bulls=0;
+
+	if (verifyEntry(userNumber)===false){
+		console.log(userNumber+" - Неправильні дані\r\n");
+	} 
+	else{
+
+	
+		for (let n=0; n<4; n++){
+			for (let i=0; i<4; i++){
+				if (userNumber.charAt(i) === rnd.charAt(n) && i==n){
+					bulls++;
+				}
+				else {
+					if(userNumber.charAt(i) === rnd.charAt(n)) {
+						cows++;
+					}
+				}
+			}
+		}
+		counter++;
+		if (bulls===4){
+			console.log(userNumber+" - Загадане число"+" \r\n"+ "Ви виграли, кількість спроб: "+counter+" \r\n");
+
+			counter=0;
+    	}
+		else{
+        	console.log(userNumber+ " - " + cows +" корів, "+ bulls +" биків, спроб: "+counter+"\r\n");}
+	}
+};
+
+var goal = check();
+
+for(let r=0; rnd != userNumber;r++ ){
+	var goal = check();
+}
+
--- try1.js
+++ try1.js
@@ -1,20 +1,20 @@
 var readlineSync = require('readline-sync');
 var blessed = require('blessed');
 
+var color_num_first = 0;
+var color_num_second = 0;
+var color_num_third = 0;
+var color_num_fourth = 0;
+
 var screen = blessed.screen({
 	smartCSR: true,
   });
-
-  var color_num_first = 0;
-  var color_num_second = 0;
-  var color_num_third = 0;
-  var color_num_fourth = 0;
   
-  var Colors=['blue','green','red','yellow','magenta','cyan'];
+var Colors=['blue','green','red','yellow','magenta','cyan'];
 
-  B=['right','15%','30%','45%'];
+var B=['right','15%','30%','45%'];
 
-A=[];
+var A=[];
 
   for (var i=0; i<=4;i++){
 	var box = blessed.box({
@@ -172,79 +172,94 @@ A=[];
 		return color_num_fourth;	  
 	});
 
-//-------------------------------------------------------------------
+
 	for (var i=0; i<4; i++){
 		A[i].key(["d"], function (ch, key) {
 			screen.focusNext();
 		  });
 
-	
 	};	
 
 screen.render();
+
 
 // GAME
 
 screen.key('enter', function (ch, key) {
 
 
-console.log(" \r\n");
+	if (color_num_first!=color_num_second && color_num_first!=color_num_third && color_num_first!=color_num_fourth && color_num_second!=color_num_third && color_num_second!=color_num_fourth && color_num_third!=color_num_fourth){
 
-var counter=0;
+		console.log(" \r\n");
 
+		var counter=0;
 
-rnd=color_num_first.toString()+color_num_second.toString()+color_num_third+color_num_fourth.toString();
+		rnd=color_num_first.toString()+color_num_second.toString()+color_num_third+color_num_fourth.toString();
 
-//console.log("Загадане число "+rnd);
+		//console.log("Загадане число "+rnd);
 
-function verifyEntry(number){	
-	verif=true;
-	number=number.toString();	
-	if (number.length!=4){
-		verif=false;
-	}
-	return verif;
-}
+		function verifyEntry(number){
+			number=number.toString();
+			verif=true;
+			for (var i=0;i<4;i++){
+				if (number.lastIndexOf(number.charAt(i))!=i){
+				verif=false;
+				break;
+				}	
+			}
+			if (number.length!=4){
+				verif=false;
+			}
+			return verif;
+		}
 
-function check(){  	
+		function check(){  	
 
-	userNumber = readlineSync.question('Enter number: ');
+			userNumber = readlineSync.question('Enter number: ');
 
-	var cows=0;
-	var bulls=0;
+			var cows=0;
+			var bulls=0;
 
-	if (verifyEntry(userNumber)===false){
-		console.log(userNumber+" - Неправильні дані\r\n");
-	} 
-	else{
+			if (verifyEntry(userNumber)===false){
+				console.log(userNumber+" - Неправильні дані\r\n");
+			} 
+			else{
+			rnd=rnd.toString();
 	
-		for (let n=0; n<4; n++){
-			for (let i=0; i<4; i++){
-				if (userNumber.charAt(i) === rnd.charAt(n) && i==n){
-					bulls++;
-				}
-				else {
-					if(userNumber.charAt(i) === rnd.charAt(n)) {
+			for (let n=0; n<4; n++){
+				for (let i=0; i<4; i++){
+					if (userNumber.charAt(i) === rnd.charAt(n) && i==n){
+						bulls++;
+					}
+					else {
+						if(userNumber.charAt(i) === rnd.charAt(n)) {
 						cows++;
+						}
 					}
 				}
 			}
-		}
-		counter++;
-		if (bulls===4){
-			console.log(userNumber+" - Загадане число"+" \r\n"+ "Ви виграли, кількість спроб: "+counter+" \r\n");
+			counter++;
+			if (bulls===4){
+				console.log(userNumber+" - Загадане число"+" \r\n"+ "Ви виграли, кількість спроб: "+counter+" \r\n");
 
-			counter=0;
-    	}
-		else{
-        	console.log(userNumber+ " - " + cows +" корів, "+ bulls +" биків, спроб: "+counter+"\r\n");}
-	}
-};
+				counter=0;
+    		}
+			else{
+        		console.log(userNumber+ " - " + cows +" корів, "+ bulls +" биків, спроб: "+counter+"\r\n");}
+			}
+		};
 
-var goal = check();
-
-for(let r=0; rnd != userNumber;r++ ){
 		var goal = check();
+
+		for(let r=0; rnd != userNumber;r++ ){
+			var goal = check();
+		}
+
+	}
+	else{
+		console.log(" \r\n");
+		console.log('Неправильні дані');
 	}
 
 });
+
